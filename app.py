79e18147b3c43ae9b3d3dc1094856140773dc35f
sammygelman <sammygelman@gmail.com>
2023-02-08 15:49:07 +0200

    trying something new

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from load_and_run_embedding import sentiment
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    sarcasm = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Sentence %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        sentence_content = request.form['content']
+
+        new_sentence = Todo(content=sentence_content)
 
         try:
-            db.session.add(new_task)
+            db.session.add(new_sentence)
+            db.session.commit()
+            # return redirect('/')
+        except:
+            return 'There was an issue checking that sentence'
+
+        sarcasm_content = request.form['sarcasm']
+        new_sarcasm = sentiment(new_sentence)
+
+        try:
+            db.session.add(new_sarcasm)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your task'
-
+            return "There was a problem measuring the sarcasm"
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
@@ -35,14 +48,15 @@ def index():
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    sentence_to_delete = Todo.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(sentence_to_delete)
         db.session.commit()
         return redirect('/')
     except:
         return 'There was a problem deleting that task'
+
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
--- load_and_run_embedding.py
+++ load_and_run_embedding.py
@@ -15,152 +15,156 @@ import os
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser(
-                                 prog="sentiment.py",
-                    description="write string and output sentiment")
- 
-parser.add_argument('sentence', type=str,
-                    help="string to test sentiment")
- 
-args = parser.parse_args() 
- 
-sentence = args.sentence
-
+# parser = argparse.ArgumentParser(
+#                                  prog="sentiment.py",
+#                     description="write string and output sentiment")
+#  
+# parser.add_argument('sentence', type=str,
+#                     help="string to test sentiment")
+#  
+# args = parser.parse_args() 
+#  
+# sentence = args.sentence
+#
 # Suppress TensorFlow output by setting the environment variable
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-tf.get_logger().setLevel('FATAL')
 
-# sent = input("Write a sentence we can check how sarcastic it is: ")
+def sentiment(sentence):
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    tf.get_logger().setLevel('FATAL')
 
-#load data
-with open('sarcasm.json', 'r') as f:
-    datastore = json.load(f)
+    # sent = input("Write a sentence we can check how sarcastic it is: ")
 
-#create lists to store data
-sentences = []
-labels = []
+    #load data
+    with open('sarcasm.json', 'r') as f:
+        datastore = json.load(f)
 
-#seperate data into lists
-for item in datastore:
-    sentences.append(item['headline'])
-    labels.append(item['is_sarcastic'])
+    #create lists to store data
+    sentences = []
+    labels = []
 
-#set training size
-percent_training = 0.8
-training_size = int(percent_training*len(sentences))
+    #seperate data into lists
+    for item in datastore:
+        sentences.append(item['headline'])
+        labels.append(item['is_sarcastic'])
 
-#Split data and labels to training and testing sets
-training_sentences = sentences[0:training_size]
-testing_sentences = sentences[training_size:]
+    #set training size
+    percent_training = 0.8
+    training_size = int(percent_training*len(sentences))
 
-training_labels = labels[0:training_size]
-testing_labels = labels[training_size:]
+    #Split data and labels to training and testing sets
+    training_sentences = sentences[0:training_size]
+    testing_sentences = sentences[training_size:]
 
-#Tokenize data
-#Note: We only tokenize data on the training set, it is important that the model knows nothing of the testing data to reflect real world use
+    training_labels = labels[0:training_size]
+    testing_labels = labels[training_size:]
 
-#Parameters
-vocab_size = 10000
-embedding_dim = 16
-max_length = 100
-trunc_type = 'post'
-padding_type =  "post"
-oov_tok = "<OOV>"
+    #Tokenize data
+    #Note: We only tokenize data on the training set, it is important that the model knows nothing of the testing data to reflect real world use
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-tokenizer.fit_on_texts(training_sentences)
+    #Parameters
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 100
+    trunc_type = 'post'
+    padding_type =  "post"
+    oov_tok = "<OOV>"
 
-word_index = tokenizer.word_index
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(training_sentences)
 
-training_sequences = tokenizer.texts_to_sequences(training_sentences)
-training_padded = pad_sequences(training_sequences,  
-                                maxlen=max_length, padding=padding_type, 
-                                truncating=trunc_type)
+    word_index = tokenizer.word_index
 
-testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
-testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
-                                padding=padding_type, truncating=trunc_type)
+    training_sequences = tokenizer.texts_to_sequences(training_sentences)
+    training_padded = pad_sequences(training_sequences,  
+                                    maxlen=max_length, padding=padding_type, 
+                                    truncating=trunc_type)
 
-
-#Load list of embeddings from tsv files saved from past training
-#Check to see that files exist
-vec_file = [s for s in os.listdir() if s.startswith("vecs.tsv")]
-meta_file = [s for s in os.listdir() if s.startswith("meta.tsv")]
-embeddings_index = {}
-if len(vec_file) != 0 and len(meta_file) != 0:
-    vecs = []
-    with open(vec_file[0]) as f:
-        for line in f:
-            vec = line.split('\t')
-            vec[-1] = vec[-1].strip()
-            vecs.append(vec)
-    words = []
-    with open(meta_file[0]) as f:
-        for line in f:
-            word = line.split('\n')
-            words.append(word[0])
-else:
-    print("Missing pre_trained embeddings!")
-    exit()
-
-for i in range(len(vecs)):
-    embeddings_index[words[i]] = vecs[i]
-
-#Here we are going to load the pretrained embedding and 
-embedding_matrix = np.zeros((vocab_size, embedding_dim))
-for word, i in word_index.items():
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None:
-        embedding_matrix[i] = embedding_vector
-
-#Define an embedding layer which incorporates the embedding matrix. 
-pretrained_embedding_layer = tf.keras.layers.Embedding(vocab_size,
-                                                       embedding_dim,
-                                                       embeddings_initializer=tf.keras.initializers.Constant(embedding_matrix),
-                                                       input_length=max_length,
-)
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size,
-                              embedding_dim,
-                              input_length=max_length,
-                              ),
-    tf.keras.layers.GlobalAveragePooling1D(),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-
-model.load_weights("weights/cp.ckpt")
+    testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    testing_padded = pad_sequences(testing_sequences, maxlen=max_length,
+                                    padding=padding_type, truncating=trunc_type)
 
 
-new_model = tf.keras.Sequential([pretrained_embedding_layer] + model.layers[1:])
+    #Load list of embeddings from tsv files saved from past training
+    #Check to see that files exist
+    vec_file = [s for s in os.listdir() if s.startswith("vecs.tsv")]
+    meta_file = [s for s in os.listdir() if s.startswith("meta.tsv")]
+    embeddings_index = {}
+    if len(vec_file) != 0 and len(meta_file) != 0:
+        vecs = []
+        with open(vec_file[0]) as f:
+            for line in f:
+                vec = line.split('\t')
+                vec[-1] = vec[-1].strip()
+                vecs.append(vec)
+        words = []
+        with open(meta_file[0]) as f:
+            for line in f:
+                word = line.split('\n')
+                words.append(word[0])
+    else:
+        print("Missing pre_trained embeddings!")
+        exit()
 
-training_padded = np.array(training_padded)
-training_labels = np.array(training_labels)
-testing_padded = np.array(testing_padded)
-testing_labels = np.array(testing_labels)
+    for i in range(len(vecs)):
+        embeddings_index[words[i]] = vecs[i]
 
-num_epochs = 30
+    #Here we are going to load the pretrained embedding and 
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
 
-# Compile and train the model
-new_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-initial_epoch = 0
+    #Define an embedding layer which incorporates the embedding matrix. 
+    pretrained_embedding_layer = tf.keras.layers.Embedding(vocab_size,
+                                                           embedding_dim,
+                                                           embeddings_initializer=tf.keras.initializers.Constant(embedding_matrix),
+                                                           input_length=max_length,
+    )
 
-e = model.layers[0]
-weights = e.get_weights()[0]
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size,
+                                  embedding_dim,
+                                  input_length=max_length,
+                                  ),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
 
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+    model.load_weights("weights/cp.ckpt")
 
-def decode_sentence(text):
-    return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
-# sentence = ["Well that was a fantastic idea, wish I thought of it myself","granny starting to fear spiders in the garden might be real","this is meant to be a non-sarcastic comment", "game of thrones season finale showing this sunday night"]
-# sequences = tokenizer.texts_to_sequences(sentence)
+    new_model = tf.keras.Sequential([pretrained_embedding_layer] + model.layers[1:])
 
-# sequences = tokenizer.texts_to_sequences([sent])
-sequences = tokenizer.texts_to_sequences([sentence])
-padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-if new_model.predict(padded)[0][0] >= 0.5:
-    print("That was probably sarcastic")
-else:
-    print("Didn't seem sarcastic to me!")
+    training_padded = np.array(training_padded)
+    training_labels = np.array(training_labels)
+    testing_padded = np.array(testing_padded)
+    testing_labels = np.array(testing_labels)
+
+    num_epochs = 30
+
+    # Compile and train the model
+    new_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    initial_epoch = 0
+
+    e = model.layers[0]
+    weights = e.get_weights()[0]
+
+    reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+    def decode_sentence(text):
+        return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+    # sentence = ["Well that was a fantastic idea, wish I thought of it myself","granny starting to fear spiders in the garden might be real","this is meant to be a non-sarcastic comment", "game of thrones season finale showing this sunday night"]
+    # sequences = tokenizer.texts_to_sequences(sentence)
+
+    # sequences = tokenizer.texts_to_sequences([sent])
+    sequences = tokenizer.texts_to_sequences([sentence])
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    if new_model.predict(padded)[0][0] >= 0.5:
+        output = "That was probably sarcastic"
+    else:
+        output = "Didn't seem sarcastic to me!"
+
+    return output
